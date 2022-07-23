ee5956422b8c6d635cca5eab83a1ff92dfeb5156
duc0867 <vietv2k5@gmail.com>
2022-07-23 16:52:45 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/getUser', function(req, res, next) {
+  fs.readFile('data/data.txt', function (err, data) {
+    if (err != null)
+    {
+      res.send(err.message);
+    }
+    else
+    {
+      res.send(data.toString());
+    }
+  });
+});
+
+module.exports = router;
